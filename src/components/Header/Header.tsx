c4e9fffff49bec4475afa4854ae1468a014995a7
Sergi Casquero <sergicasquero@gmail.com>
2023-02-19 00:30:20 +0100

    Add styles to Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,14 @@
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import HeaderStyled from "./HeaderStyled";
 
 const Header = (): JSX.Element => {
   return (
-    <>
+    <HeaderStyled>
       <i className="fa-solid fa-bars"></i>
       <h1>Robot Manager</h1>
       <i className="fa-solid fa-share-nodes"></i>
       <i className="fa-solid fa-magnifying-glass"></i>
-    </>
+    </HeaderStyled>
   );
 };
 
