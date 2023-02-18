0d8307c9b60ce2ff605cd8937df4766b773742a8
Sergi Casquero <sergicasquero@gmail.com>
2023-02-19 00:08:46 +0100

    Add Header component structure

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+const Header = (): JSX.Element => {
+  return (
+    <>
+      <i className="fa-solid fa-bars"></i>
+      <h1>Robot Manager</h1>
+      <i className="fa-solid fa-share-nodes"></i>
+      <i className="fa-solid fa-magnifying-glass"></i>
+    </>
+  );
+};
+
+export default Header;
